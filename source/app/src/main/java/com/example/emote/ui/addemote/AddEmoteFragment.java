edca173e6922a7538716d247de3dae4f56e35e2f
dinulade101 <dinulasd@gmail.com>
2019-11-01 11:31:39 -0600

    add emote screen basic version working

--- source/app/src/main/java/com/example/emote/ui/addemote/AddEmoteFragment.java
+++ source/app/src/main/java/com/example/emote/ui/addemote/AddEmoteFragment.java
@@ -1,12 +1,18 @@
 package com.example.emote.ui.addemote;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -15,6 +21,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.emote.FireStoreHandler;
+import com.example.emote.MainActivity;
 import com.example.emote.R;
 import com.example.emote.EmotionEvent;
 import com.example.emote.Situation;
@@ -36,6 +43,9 @@ public class AddEmoteFragment extends Fragment {
     private EditText textTimeField;
     private EditText textReasonField;
     private EditText textSituationField;
+    DatePickerDialog datePicker;
+    TimePickerDialog timePicker;
+    Spinner situationSpinner;
 
     public void initializeViews() {
 
@@ -61,6 +71,7 @@ public class AddEmoteFragment extends Fragment {
         textDateField = root.findViewById(R.id.text_date_field);
         textTimeField = root.findViewById(R.id.text_time_field);
         textReasonField = root.findViewById(R.id.text_reason_field);
+        situationSpinner = root.findViewById(R.id.spinner_situation);
 
 //        FireStoreHandler fsh = new FireStoreHandler("john123");
 //        EmotionEvent emoteEvent1 = new EmotionEvent("Happy", Situation.FEW_PEOPLE, "Good food");
@@ -70,12 +81,42 @@ public class AddEmoteFragment extends Fragment {
 //        fsh.addEmote(emoteEvent2);
 //        fsh.addEmote(emoteEvent3);
 
+        situationSpinner.setAdapter(new ArrayAdapter<Situation>(getContext(), android.R.layout.simple_spinner_item, Situation.values()));
+
         textDateField.setInputType(InputType.TYPE_NULL);
         textDateField.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final Calendar cldr = Calendar.getInstance();
                 int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+
+                datePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        textDateField.setText(String.format("%02d:%02d", i, i1));
+                    }
+                }, year, month, day);
+                datePicker.show();
+            }
+        });
+
+        textTimeField.setInputType(InputType.TYPE_NULL);
+        textTimeField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr = Calendar.getInstance();
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minute = cldr.get(Calendar.MINUTE);
+
+                timePicker = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        textTimeField.setText(i + ":" + i1);
+                    }
+                }, hour, minute, true);
+                timePicker.show();
             }
         });
 
