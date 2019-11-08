e5cb1bfffa12f50a9ce26c562b2944fb3b8d5baa
Kai Bailey <kbailey1@ualberta.ca>
2019-11-08 11:51:38 -0700

    Added ui tests for the profile page

--- source/app/src/androidTest/java/com/example/emote/ListEmoteTest.java
+++ source/app/src/androidTest/java/com/example/emote/ListEmoteTest.java
@@ -14,19 +14,21 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ListEmoteTest {
 
-    private String invalidUserName = "invalidUserName";
-    private String invalidPassword = "invalid123";
-    private String validUserName = "testuser";
-    private String validPassword = "password123";
+    private String validUserName = "testuser4";
 
 
     @Rule
@@ -35,24 +37,48 @@ public class ListEmoteTest {
 
     @Before
     public void setup(){
-     EmoteApplication.setUsername("testuser4");
+     EmoteApplication.setUsername(validUserName);
     }
 
     @Test
-    public void testAddEmote(){
-
-        onView(withId(R.id.text_date_field))
-                .perform(PickerActions.setDate(2019, 11, 20));
-        onView(withId(R.id.text_time_field))
-                .perform(PickerActions.setTime(11, 30));
+    public void testReasonField(){
         onView(withId(R.id.text_reason_field))
-                .perform(typeText("I saw a puppy!"), closeSoftKeyboard());
-        onView(withId(R.id.submitButton)).perform(click());
-
-//        // Check that the text was changed.
-//        onView(withId(R.id.textToBeChanged))
-//                .check(matches(withText(stringToBetyped)));
+                .perform(typeText("I saw a puppy!"), closeSoftKeyboard())
+        .check(matches(withText("I saw a puppy!")));
     }
 
+    @Test
+    public void testDateField(){
+        onView(withId(R.id.text_date_field)).perform(click());
+        onView(withText("Cancel")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testTimeField(){
+        onView(withId(R.id.text_time_field)).perform(click());
+        onView(withText("Cancel")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testEmotionField(){
+        onView(withId(R.id.spinnner_emote)).perform(click());
+        onView(withText("Happy")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSituationField(){
+        onView(withId(R.id.spinner_situation)).perform(click());
+        onView(withText("Alone")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testInvalidFieldsAddEmotion(){
+        onView(withId(R.id.add_emote_scroll_view))
+                .perform(swipeUp());
+        onView(withId(R.id.submitButton)).perform(click());
+        onView(withText("Error: length=1; index=2"))
+                .inRoot(withDecorView(not(is(activityRule.getActivity()
+                        .getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
 
 }
--- source/app/src/androidTest/java/com/example/emote/LoginSignupTest.java
+++ source/app/src/androidTest/java/com/example/emote/LoginSignupTest.java
@@ -1,30 +1,18 @@
 package com.example.emote;
 
-import android.content.ComponentName;
-
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.idling.CountingIdlingResource;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.runner.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.InstrumentationRegistry.getTargetContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -34,41 +22,39 @@ public class LoginSignupTest {
     private String invalidPassword = "invalid123";
     private String validUserName = "testuser4";
     private String validPassword = "password123";
+    private String invalidLengthPassword = "a";
 
 
     @Rule
-    public IntentsTestRule<LoginActivity> mLoginActivityActivityTestRule =
+    public IntentsTestRule<LoginActivity> activityTestRule =
             new IntentsTestRule<>(LoginActivity.class);
 
 
-
     @Test
-    public void invlaidLogin() {
+    public void unregisiteredUserInvlaidLogin() {
         onView(withId(R.id.input_username))
                 .perform(typeText(invalidUserName), closeSoftKeyboard());
         onView(withId(R.id.input_password))
                 .perform(typeText(invalidPassword), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
+    }
 
+    @Test
+    public void invlaidUserNameLengthLogin() {
+        onView(withId(R.id.input_username))
+                .perform(typeText(invalidUserName), closeSoftKeyboard());
+        onView(withId(R.id.input_password))
+                .perform(typeText(invalidLengthPassword), closeSoftKeyboard());
+        onView(withId(R.id.btn_login)).perform(click());
     }
 
     @Test
     public void validLogin() {
-
         onView(withId(R.id.input_username))
                 .perform(typeText(validUserName), closeSoftKeyboard());
         onView(withId(R.id.input_password))
                 .perform(typeText(validPassword), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
-
-                // Check that the text was changed.
-//        onView(withId(R.id.))
-//                .check(matches(withText("")));
-
-        intended(hasComponent(LoginActivity.class.getName()));
-
-
-
     }
 
 }
