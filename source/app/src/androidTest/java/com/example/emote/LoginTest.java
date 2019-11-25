44939702dad3e0a1a2cee3a7148af6b4048660f6
dinulade101 <dinulasd@gmail.com>
2019-11-24 23:16:29 -0700

    login tests fixed

--- source/app/src/androidTest/java/com/example/emote/LoginTest.java
+++ source/app/src/androidTest/java/com/example/emote/LoginTest.java
@@ -14,9 +14,15 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Test class for the login activities.
@@ -44,15 +50,26 @@ public class LoginTest {
         onView(withId(R.id.input_password))
                 .perform(typeText(invalidPassword), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
+
+        onView(withText("Authentication failed."))
+                .inRoot(withDecorView(not(is(activityTestRule.getActivity()
+                        .getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
 
     @Test
     public void invlaidUserNameLengthLogin() {
+        CountingIdlingResource idlingResource = activityTestRule.getActivity().returnIdlingResource();
+        IdlingRegistry.getInstance().register(idlingResource);
+
         onView(withId(R.id.input_username))
                 .perform(typeText(invalidUserName), closeSoftKeyboard());
         onView(withId(R.id.input_password))
                 .perform(typeText(invalidLengthPassword), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
+
+        onView(withText("Authentication failed."))
+                .inRoot(withDecorView(not(is(activityTestRule.getActivity()
+                        .getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
 
     @Test
