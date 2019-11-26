7dea29e2db13cc81759015af390b180b64df026e
dinulade101 <dinulasd@gmail.com>
2019-11-26 10:10:07 -0700

    fixed wrong index issue

--- source/app/src/androidTest/java/com/example/emote/ListEmoteTest.java
+++ source/app/src/androidTest/java/com/example/emote/ListEmoteTest.java
@@ -6,12 +6,18 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 
 /**
  * Test class for the list emotion fragment.
@@ -41,6 +47,13 @@ public class ListEmoteTest {
     public void testFilterSelector(){
         onView(withId(R.id.spinner)).perform(click());
         onView(withText("Happy")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testFilterHappy(){
+        onView(withId(R.id.spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Happy"))).perform(click());
+        onView(withId(R.id.spinner)).check(matches(withSpinnerText(containsString("Happy"))));
     }
 
 }
